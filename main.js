b83f8df518dbe4a4468849220b43d5f6328a7207
Thalion1 <gustav.kodehode@gmail.com>
2026-02-26 09:00:38 +0100

    a

---
+++ main.js
@@ -0,0 +1,14 @@
+function color_changer() {
+    var style = window.getComputedStyle(document.body).getPropertyValue('--background-color');
+    // var colors = window.getComputedStyle(document.body).getPropertyValue('--background-color');
+    var r = document.querySelector(':root');
+    if (style === 'white') {
+        r.style.setProperty('--background-color', 'green');
+        console.log('test white');
+        return
+    } else {
+        document.documentElement.style.cssText = "--background-color: black";
+        console.log('test not white');
+        return
+    }
+}
